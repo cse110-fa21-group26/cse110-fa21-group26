cc5e4033fa943b2154cfa8e69b02471f785b3f0a
kylenakai <ktnakai@ucsd.edu>
2021-12-01 22:01:03 -0800

    JSDocs implement and comments on main

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -25,6 +25,9 @@ const router = new Router(function () {
 window.addEventListener('DOMContentLoaded', init);
 
 // Initialize function, begins all of the JS code in this file
+/**
+ * Initialize function, begins all of the JS code in this file
+ */
 async function init() {
     initializeServiceWorker();
     createRecipeCards(0);
@@ -33,6 +36,9 @@ async function init() {
 }
 
 /* Dropdown Functionality */
+/**
+ * Enable functionality of the dropdown menu
+ */
 function toggleNav() {
     if (document.getElementById("mySidebar").getAttribute("open") == "true") {
         document.getElementById("mySidebar").style.width = "250px";
@@ -82,6 +88,7 @@ for (let i = 0; i < categories.length; i++) {
 /**
  * Generates the <recipe-card> elements from the fetched recipes and
  * appends them to the page
+ * @param category name of the category to create the card
  */
 function createRecipeCards(category) {
     for (let i = 0; i < recipeData.length; i++) {
@@ -104,6 +111,9 @@ function createRecipeCards(category) {
     }
 }
 
+/**
+ * specify the navigation for our pages
+ */
 function bindPopstate() {
     window.addEventListener("popstate", (event) => {
         if (event.state != null && event.state.state_page != null) router.navigate(event.state.state_page, true);
@@ -111,6 +121,12 @@ function bindPopstate() {
     })
 }
 
+/**
+ * This function binds the functionality of our recipe cards to open when clicked on
+ * @param {*} recipeCard recipe card html element that represents the recipe on the page
+ * @param {*} pageName name of the page to route to
+ * @param {*} jsonData Json data of the recipe we want to open
+ */
 function bindRecipeCard(recipeCard, pageName, jsonData) {
     recipeCard.addEventListener('click', e => {
         //if (e.path[0].nodeName == 'A') return;
@@ -121,6 +137,10 @@ function bindRecipeCard(recipeCard, pageName, jsonData) {
     });
 }
 
+/**
+ * Displays the content of the opened recipe card 
+ * @param {*} jsonData data of the recipe to display when opened
+ */
 function openRecipe(jsonData){
     let body = document.getElementById("body");
     let priorState = document.getElementById("main");
@@ -185,6 +205,9 @@ createNewRecipe.addEventListener('click', event => {
 
 });
 
+/**
+ * Adds functionality to the escape key to navigate back to the home page
+ */
 function bindEscKey() {
     document.addEventListener("keydown", (event) => {
         if (event.key == "Escape") router.navigate("home", false);
