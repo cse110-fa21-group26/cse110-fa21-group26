42eacfd3ffb4902d66808e2b5434025fd638c1c8
Arman Ommid <armanommid@gmail.com>
2021-12-11 00:43:16 -0800

    compelted unit testing framework

--- source/scripts/test/test.js
+++ source/scripts/test/test.js
@@ -1,6 +1,6 @@
 
 const { recipeData } = require('./AllRecipes.js');
-const { add, subtract, multiply, spoonacularSearch, strictSearch, looseSearch, searchJSON} = require("./test-functions");
+const { add, subtract, multiply, spoonacularSearch, strictSearch, looseSearch} = require("./test-functions");
 
 var item1 = "Easy Sheet Pan Pancakes";
 var item1vegan = item1 + ": vegan";
